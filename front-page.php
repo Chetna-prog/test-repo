f69d9e97e1b4beb45ea896c3b0078e039afac46c
Chetna-prog <74489353+Chetna-prog@users.noreply.github.com>
2020-11-15 21:40:01 +0530

    commit
    commit

--- front-page.php
+++ front-page.php
@@ -1,44 +1,172 @@
-<?php
-/**
- * The main template file
- **/
-
-get_header();
-?>
-
-	<main id="primary" class="site-main">
-
-		<?php
-		if ( have_posts() ) :
-
-			
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>Header Design</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="<?php  echo get_stylesheet_directory_uri()?>/css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="<?php  echo get_stylesheet_directory_uri()?>/css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="<?php  echo get_stylesheet_directory_uri()?>/css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="images/fevicon.png" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- Tweaks for older IEs-->
+      <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+       <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;1,300&display=swap" rel="stylesheet">
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+      <!-- loader  -->
+      <!--<div class="loader_bg">
+         <div class="loader"><img src="images/loading.gif" alt="#" /></div>
+      </div>-->
+      <!-- end loader -->
+      <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="header">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
+                     <div class="full">
+                        <div class="center-desk">
+                           <div class="logo"> <a href="index.html"><img src="<?php  echo get_stylesheet_directory_uri()?>/images/logo.png" alt="#"></a> </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <div class="menu-area">
+                        <div class="limit-box">
+                           <nav class="main-menu">
+                              <ul class="menu-area-main">
+                                 <li class="active"> <a href="#">Home</a> </li>
+                                 <li> <a href="#about">About</a> </li>
+                                 <li><a href="#plant">Plant</a></li>
+                                 <li><a href="#gallery">Gallery</a></li>
+                                 <li><a href="#contact">Contact Us</a></li>
+                                  <li class="last"><a href="#"><i class="fa fa-search"></i></a></li>
+                              </ul>
+                           </nav>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- end header inner -->
+      </header>
+      <!-- end header -->
+      <section >
+         <div id="main_slider" class="carousel slide banner-main" data-ride="carousel">
+            <ol class="carousel-indicators">
+               <li data-target="#main_slider" data-slide-to="0" class="active"></li>
+               <li data-target="#main_slider" data-slide-to="1"></li>
+               <li data-target="#main_slider" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+               <div class="carousel-item active">
+                  <div class="container">
+                     <div class="row marginii">
+                        <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                           <div class="carousel-caption ">
+                              <h4>Uncatgeorized</h4>
+                              <h1><strong class="color">Tinci dunt vitae sem dor</strong></h1>
+                              <p>By wpin</p>
+                           </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                           <div class="img-box">
+                               <h2>What's hot!</h2>
+                              <div class="row">
+                                  <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                                      <figure><img src="<?php  echo get_stylesheet_directory_uri()?>/images/gyufyufyu.png" alt="img"/></figure>
+                                  </div>
+                                  <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                                      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                                      <p class="date-style">June 11, 2020</p>
+                                  </div>
+                               </div>  
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="container">
+                     <div class="row marginii">
+                        <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                           <div class="carousel-caption ">
+                              <h4>Uncatgeorized</h4>
+                              <h1><strong class="color">Tinci dunt vitae sem dor</strong></h1>
+                              <p>By wpin</p>
+                           </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                           <div class="img-box">
+                               <h2>What's hot!</h2>
+                              <div class="row">
+                                  <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                                      <figure><img src="<?php  echo get_stylesheet_directory_uri()?>/images/gyufyufyu.png" alt="img"/></figure>
+                                  </div>
+                                  <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                                      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                                      <p class="date-style">June 11, 2020</p>
+                                  </div>
+                               </div>  
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel-item">
+                  <div class="container">
+                     <div class="row marginii">
+                        <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                           <div class="carousel-caption ">
+                              <h4>Uncatgeorized</h4>
+                              <h1><strong class="color">Tinci dunt vitae sem dor</strong></h1>
+                              <p>By wpin</p>
+                           </div>
+                        </div>
+                        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                           <div class="img-box">
+                               <h2>What's hot!</h2>
+                              <div class="row">
+                                  <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                                      <figure><img src="<?php  echo get_stylesheet_directory_uri()?>/images/gyufyufyu.png" alt="img"/></figure>
+                                  </div>
+                                  <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                                      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                                      <p class="date-style">June 11, 2020</p>
+                                  </div>
+                               </div>  
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- Javascript files-->
+      <script src="<?php  echo get_stylesheet_directory_uri()?>/js/jquery.min.js"></script>
+      <script src="<?php  echo get_stylesheet_directory_uri()?>/js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- javascript --> 
+   </body>
+</html>
--- template-parts/content.php
+++ template-parts/content.php
@@ -20,16 +20,11 @@
 
 		if ( 'post' === get_post_type() ) :
 			?>
-			<div class="entry-meta">
-				<?php
-				vlog_posted_on();
-				vlog_posted_by();
-				?>
-			</div><!-- .entry-meta -->
+			
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php vlog_post_thumbnail(); ?>
+	
 
 	<div class="entry-content">
 		<?php
@@ -58,6 +53,6 @@
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
-		<?php vlog_entry_footer(); ?>
+		<?php //vlog_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
